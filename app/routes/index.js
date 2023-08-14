f944b9b9a7d1d658016e6555e1e01b5ca31351bf
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-14 12:50:29 -0300

    Organizing templates by file types

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let formidable = require('formidable')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload',(req,res)=>{
+
+  let form = new formidable.IncomingForm({
+
+     uploadDir:'./upload', 
+     keepExtensions:true
+
+  }); 
+
+  form.parse(req,(err,fields, files )=>{
+
+    res.json({
+      
+      files 
+
+
+    });
+
+  });
+
+})
 
 module.exports = router;
